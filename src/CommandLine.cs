1706d68c07cda037448bd244091ad4fa0329ab80
Maxime Labelle <maxime.labelle@moskitos.fr>
2017-03-16 10:28:18 +0100

    New - AppVeyor configuration.

---
+++ src/CommandLine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NDesk.Options;
+
+namespace make
+{
+    public class CommandLine
+    {
+        public string Program { get; set; }
+        public string OutputFile { get; set; }
+        public string InputFile { get; set; }
+        public string[] Arguments { get; set; }
+
+
+        private CommandLine()
+        {
+        }
+
+        public static CommandLine Parse(string[] args)
+        {
+            var commandLine = new CommandLine();
+
+            var options = new OptionSet
+            {
+                { "p|program=", v => commandLine.Program = v },
+                { "o|out=", v => commandLine.OutputFile = v },
+            };
+
+            try
+            {
+                var remaining = options.Parse(args);
+                commandLine.ParseRemainingArguments(remaining);
+            }
+            catch (OptionException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+
+            return commandLine;
+        }
+
+        private void ParseRemainingArguments(List<string> remaining)
+        {
+            var input = "";
+            var options = new List<string>();
+
+            foreach (var arg in remaining)
+            {
+                if (arg.StartsWith("/") || arg.StartsWith("-"))
+                    options.Add(arg);
+                else if (File.Exists(arg))
+                    input = arg;
+                else
+                    options.Add(arg);
+            }
+
+            InputFile = input;
+            Arguments = options.ToArray();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace make
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var commandLine = CommandLine.Parse(args);
+
+            var echo = new StringBuilder();
+            echo.AppendFormat("{0} ", commandLine.Program);
+            foreach (var arg in commandLine.Arguments)
+                echo.AppendFormat("{0} ", arg);
+            if (!String.IsNullOrEmpty(commandLine.OutputFile))
+                echo.AppendFormat("/out:{0} ", commandLine.OutputFile);
+            if (!String.IsNullOrEmpty(commandLine.InputFile))
+                echo.Append(commandLine.InputFile);
+
+            Console.WriteLine(echo);
+
+            var output = File.Exists(commandLine.OutputFile)
+                ? File.GetLastWriteTimeUtc(commandLine.OutputFile)
+                : DateTime.MinValue
+                ;
+
+            var input = File.Exists(commandLine.InputFile)
+                ? File.GetLastWriteTimeUtc(commandLine.InputFile)
+                : DateTime.MaxValue
+                ;
+
+            if (input > output)
+            {
+                Console.WriteLine($"Building {commandLine.OutputFile} because {commandLine.InputFile} is more recent...");
+                var process = Process.Start("dotnet", echo.ToString());
+                process.WaitForExit();
+            }
+        }
+    }
+}
